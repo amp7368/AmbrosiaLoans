e7eac616d916287483d492f325297f972c8b4d4d
Aaron Peterham <47309189+amp7368@users.noreply.github.com>
2025-04-13 17:33:36 -0400

    Cleanup unused SQL queries
    Signed-off-by: Aaron Peterham <47309189+amp7368@users.noreply.github.com>

--- sql/simulate/find_bad_balances.sql
+++ sql/simulate/find_bad_balances.sql
@@ -26,18 +26,3 @@ WHERE (SELECT balance
        WHERE c.id = client_id
        ORDER BY date DESC, event DESC
        LIMIT 1) BETWEEN -1 AND -4096 * 30;
-
-SELECT *
-FROM adjust_balance
-WHERE client_id = 296;
-
-SELECT *
-FROM adjust_loan
-WHERE loan_id = 272
-ORDER BY date DESC;
-
-SELECT *
-FROM loan
-WHERE id = 293;
-
-
--- sql/simulate/investor_adjustments.sql
+++ sql/simulate/investor_adjustments.sql
@@ -1,10 +1,10 @@
-SELECT round(SUM(delta) / 4096) AS delta, client_id, minecraft_username, balance_invest_amount
+SELECT ROUND(SUM(delta) / 4096) AS delta, client_id, minecraft_username, balance_invest_amount
 FROM client_invest_snapshot
-         left join client on client_invest_snapshot.client_id = client.id
+         LEFT JOIN client ON client_invest_snapshot.client_id = client.id
 WHERE event IN ('ADJUST_DOWN', 'ADJUST_UP')
 GROUP BY client_id, minecraft_username, balance_invest_amount
 HAVING SUM(delta) != 0
-order by delta;
+ORDER BY delta;
 
 SELECT SUM(delta) / 4096 / 64, event
 FROM client_invest_snapshot
@@ -12,51 +12,71 @@ GROUP BY event
 ORDER BY event;
 
 
-select to_char(date, 'MM/DD/YYYY')     as date,
-       ((select sum(delta)
-         from (SELECT SUM(delta) AS delta
+SELECT TO_CHAR(date, 'MM/DD/YYYY')     AS date,
+       ((SELECT SUM(delta)
+         FROM (SELECT SUM(delta) AS delta
                FROM client_invest_snapshot other
                WHERE event IN ('ADJUST_DOWN', 'ADJUST_UP')
-                 and other.date <= orig.date
+                 AND other.date <= orig.date
                GROUP BY client_id) q
-         where delta < 0) +
-        (select sum(delta)
-         from (SELECT SUM(delta) AS delta
+         WHERE delta < 0) +
+        (SELECT SUM(delta)
+         FROM (SELECT SUM(delta) AS delta
                FROM client_invest_snapshot other
                WHERE event IN ('ADJUST_DOWN', 'ADJUST_UP')
-                 and other.date <= orig.date
-                 and other.date >= '06/30/2024'
+                 AND other.date <= orig.date
+                 AND other.date >= '06/30/2024'
                GROUP BY client_id) q
-         where delta > 0)) / 4096 / 64 as down,
-       ((select sum(delta)
-         from (SELECT SUM(delta) AS delta
+         WHERE delta > 0)) / 4096 / 64 AS down,
+       ((SELECT SUM(delta)
+         FROM (SELECT SUM(delta) AS delta
                FROM client_invest_snapshot other
                WHERE event IN ('ADJUST_DOWN', 'ADJUST_UP')
-                 and other.date <= orig.date
+                 AND other.date <= orig.date
                GROUP BY client_id) q
-         where delta > 0) +
-        (select sum(delta)
-         from (SELECT SUM(delta) AS delta
+         WHERE delta > 0) +
+        (SELECT SUM(delta)
+         FROM (SELECT SUM(delta) AS delta
                FROM client_invest_snapshot other
                WHERE event IN ('ADJUST_DOWN', 'ADJUST_UP')
-                 and other.date <= orig.date
-                 and other.date >= '06/30/2024'
+                 AND other.date <= orig.date
+                 AND other.date >= '06/30/2024'
                GROUP BY client_id) q
-         where delta < 0)) / 4096 / 64 as up,
-       (select sum(delta)
-        from (SELECT SUM(delta) AS delta
+         WHERE delta < 0)) / 4096 / 64 AS up,
+       (SELECT SUM(delta)
+        FROM (SELECT SUM(delta) AS delta
               FROM client_invest_snapshot
               WHERE event IN ('ADJUST_DOWN', 'ADJUST_UP')
-                and client_invest_snapshot.date < orig.date
+                AND client_invest_snapshot.date < orig.date
               GROUP BY client_id) q) / 4096 / 64
-                                       as diff,
-       (select sum(abs(delta))
-        from (SELECT SUM(delta) AS delta
+                                       AS diff,
+       (SELECT SUM(ABS(delta))
+        FROM (SELECT SUM(delta) AS delta
               FROM client_invest_snapshot
               WHERE event IN ('ADJUST_DOWN', 'ADJUST_UP')
-                and client_invest_snapshot.date < orig.date
+                AND client_invest_snapshot.date < orig.date
               GROUP BY client_id) q) / 4096 / 64
-                                       as added
-from client_invest_snapshot orig
-where orig.date > '06/30/2024'
-group by orig.date
+                                       AS added
+FROM client_invest_snapshot orig
+WHERE orig.date > '06/30/2024'
+GROUP BY orig.date;
+
+
+
+SELECT (SELECT SUM(ABS(amount)) / 4096.0 / 64 AS original
+        FROM (SELECT SUM(amount) AS amount
+              FROM adjust_balance
+              WHERE adjust_balance.event_type IN ('ADJUST_UP', 'ADJUST_DOWN')
+                AND client_id IN (SELECT id
+                                  FROM client
+                                  WHERE balance_invest_amount != 0)
+              GROUP BY client_id) q) original,
+       (SELECT SUM(ABS(amount)) / 4096.0 / 64 AS original
+        FROM (SELECT SUM(delta) AS amount
+              FROM client_invest_snapshot
+              WHERE client_invest_snapshot.event IN ('ADJUST_UP', 'ADJUST_DOWN')
+                AND client_id IN (SELECT id
+                                  FROM client
+                                  WHERE balance_invest_amount != 0)
+              GROUP BY client_id) q) current;
+
